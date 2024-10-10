34bfe5512cf86c45c3346712652d86aad3fee107
Clara Mougeot <clara.mougeot@eurecom.fr>
2024-10-10 07:14:54 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+# check if the source files are present in the working directory
+test -f "rpn1.c" && echo "Exists" || echo "Does not exist"
+test -f "rpn2.c" && echo "Exists" || echo "Does not exist"
+test -f "rpn3.c" && echo "Exists" || echo "Does not exist"
+test -f "rpn4.c" && echo "Exists" || echo "Does not exist"
+
+# compile the source files to generate an executable file per source file
 gcc rpn1.c -o rpn1
 gcc rpn2.c -o rpn2
 gcc rpn3.c -o rpn3
