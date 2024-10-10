3f15942b1fc5b48601228fb7eafb2ab6c8bbbfda
Jacem Haggui <jacem.haggui@eurecom.fr>
2024-10-10 22:14:32 +0000

    Made the the messages in build.sh more specific

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-# check if the source files are present in the working directory
-test -f "rpn1.c" && echo "Exists" || echo "Does not exist"
-test -f "rpn2.c" && echo "Exists" || echo "Does not exist"
-test -f "rpn3.c" && echo "Exists" || echo "Does not exist"
-test -f "rpn4.c" && echo "Exists" || echo "Does not exist"
-test -f "rpn5.c" && echo "Exists" || echo "Does not exist"
+# Check if the source files are present in the working directory
+test -f "rpn1.c" && echo "rpn1.c Exists" || echo "rpn1.c Does not exist"
+test -f "rpn2.c" && echo "rpn2.c Exists" || echo "rpn2.c Does not exist"
+test -f "rpn3.c" && echo "rpn3.c Exists" || echo "rpn3.c Does not exist"
+test -f "rpn4.c" && echo "rpn4.c Exists" || echo "rpn4.c Does not exist"
+test -f "rpn5.c" && echo "rpn5.c Exists" || echo "rpn5.c Does not exist"
 
 # compile the source files to generate an executable file per source file
 gcc rpn1.c -o rpn1
